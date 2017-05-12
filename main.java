77acf1c0832fbbfc2d951e5f415a4d9b39cfea78
bfkol <bfkolle@live.com>
2017-05-12 11:47:51 -0400

    -added numerous event handlers and methods -General cleanup

--- main.java
+++ main.java
@@ -5,7 +5,6 @@ import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.event.*;
 import java.util.*;
 
 /**
@@ -27,6 +26,7 @@ public class main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Calculator");
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
@@ -37,7 +37,7 @@ public class main extends Application {
 class Calculator extends VBox {
 
     //Declare each button of calculator
-    Button btCE = new Button("CE"), btC = new Button("C"), btBack = new Button("<--"),
+    public Button btCE = new Button("CE"), btC = new Button("C"), btBack = new Button("<--"),
             btDiv = new Button("/"),  bt7 = new Button("7"), bt8 = new Button("8"),
             bt9 = new Button("9"), btMult = new Button("*"), bt4 = new Button("4"),
             bt5 = new Button("5"), bt6 = new Button("6"), btSub = new Button("-"),
@@ -45,36 +45,29 @@ class Calculator extends VBox {
             btAdd = new Button("+"), btNeg = new Button("Neg"), bt0 = new Button("0"),
             btDec = new Button("."), btEquals = new Button("=");
 
-    //Text box for display
-    private Text display = new Text("0");
+    private ArrayList<Button> buttonHolder = new ArrayList<>(); //ArrayList to hold all of the buttons
 
-    //default constructor sets up calculator graphics
+    private Text display = new Text("5"); //Text box for display
+
+    private double value = 0; //Value for arithmetic purposes
+
+    private double input; //Secondary value for arithmetic purposes
+
+    private int operatorIndicator = 0; //tells equals method which operation to perform
+
+    //Default constructor sets up calculator graphics
     public Calculator() {
 
-        /*ArrayList to hold all of the buttons*/
-        ArrayList<Button> buttonHolder = new ArrayList<>();
-
-        /*Add all buttons to buttonHolder arrayList*/
+        //Add all buttons to buttonHolder arrayList
         Collections.addAll(buttonHolder, btCE, btC, btBack, btDiv, bt7, bt8, bt9, btMult, bt4, bt5, bt6, btSub, bt1,
                             bt2, bt3, btAdd, btNeg, bt0, btDec, btEquals);
 
-        //Set each button so it will grow to fit space, set to bold, and add action listners
+        //Set each button so it will grow to fit space, and set to bold
         for (int i = 0; i < buttonHolder.size(); i++) {
             buttonHolder.get(i).minWidthProperty().bind(this.prefWidthProperty().divide(4.0));
             buttonHolder.get(i).minHeightProperty().bind(this.prefHeightProperty().divide(6.0));
             buttonHolder.get(i).setFont(Font.font("sans serif", FontWeight.BOLD, 30 ));
         }
-
-        //set text as first row
-        display.setFont(Font.font("sans serif", FontWeight.BOLD, 45));
-
-        HBox row0 = new HBox();
-
-        row0.alignmentProperty().setValue(Pos.BOTTOM_RIGHT);
-        row0.minHeightProperty().bind(this.prefHeightProperty().divide(6.0));
-        row0.minWidthProperty().bind(this.prefWidthProperty());
-
-        row0.getChildren().add(display);
 
         /*Create each row of the calculator as a hbox.
          * Each row holds 4 buttons of the calculator.*/
@@ -93,11 +86,161 @@ class Calculator extends VBox {
         HBox row5 = new HBox();
         row5.getChildren().addAll(btNeg, bt0, btDec, btEquals);
 
+        //set text as first row
+        display.setFont(Font.font("sans serif", FontWeight.BOLD, 45));
+
+        HBox row0 = new HBox();
+
+        row0.alignmentProperty().setValue(Pos.BOTTOM_RIGHT);
+        row0.minHeightProperty().bind(this.prefHeightProperty().divide(6.0));
+        row0.minWidthProperty().bind(this.prefWidthProperty());
+
+        row0.getChildren().add(display);
+
         //add all hboxes
         this.getChildren().addAll(row0, row1, row2, row3, row4, row5);
+
+        //assign event handler to each button
+        btCE.setOnAction(e -> this.clearScreen());
+
+        btC.setOnAction(e -> this.clearAll());
+
+        btDiv.setOnAction(e -> this.divide());
+
+        btEquals.setOnAction(e -> this.equal(operatorIndicator));
+
+        bt9.setOnAction(e -> this.nine(9));
+
+        bt8.setOnAction(e -> this.eight(8));
+
+        bt7.setOnAction(e -> this.seven(7));
+
+        bt6.setOnAction(e -> this.six(6));
+
+        bt5.setOnAction(e -> this.five(5));
+
+        bt4.setOnAction(e -> this.four(4));
+
+        bt3.setOnAction(e -> this.three(3));
+
+        bt2.setOnAction(e -> this.two(2));
+
+        bt1.setOnAction(e -> this.one(1));
     }
 
+    //method to change the text displayed
     public void changeDisplay(double num) {
         display.setText(Double.toString(num));
+    }
+
+    public void add(double num) {
+        operatorIndicator = 4;
+        display.setText(display.getText() + " + ");
+    }
+
+    public void subtract(double num) {
+        operatorIndicator = 3;
+        display.setText(display.getText() + " - ");
+    }
+
+    public void multiply(double num) {
+        operatorIndicator = 2;
+        display.setText(display.getText() + " * ");
+    }
+
+    public void divide() {
+        operatorIndicator = 1;
+        display.setText(display.getText() + " / ");
+    }
+
+    public void equal(int num) {
+
+        switch(num) {
+
+            case 0: break;
+
+            case 1:
+                if(input == 0) {
+                    display.setText("Divide by Zero");
+                    value = 0;
+                    operatorIndicator = 0;
+                    break;
+                }
+                else {
+                    value /= input;
+                    this.changeDisplay(value);
+                    operatorIndicator = 0;
+                    break;
+                }
+
+            case 2:
+                value *= num;
+                operatorIndicator = 0;
+                break;
+
+            case 3:
+                value -= num;
+                operatorIndicator = 0;
+                break;
+
+            case 4:
+                value += num;
+                operatorIndicator = 0;
+                break;
+        }
+    }
+
+    private void clearScreen() {
+        this.changeDisplay(0);
+    }
+
+    private void clearAll() {
+        value = 0;
+        this.changeDisplay(value);
+    }
+
+    private void nine(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void eight(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void seven(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void six(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void five(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void four(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void three(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void two(double num) {
+        value = num;
+        this.changeDisplay(value);
+    }
+
+    private void one(double num) {
+        value = num;
+        this.changeDisplay(value);
     }
 }
