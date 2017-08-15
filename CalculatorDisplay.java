c44b543f2558920f94bbbc838a4f83a6375d42f1
bfkol <bfkolle@live.com>
2017-08-15 16:00:52 -0400

    -Extensively reworked logic for clearer and simpler methods -Added secondary display to to display operands at different sizes -Added two overloaded setDisplay() methods to CalculatorDisplay class to enable easier and cleaner handling of equation during computation -Added setDisplayFontSize() to CalculatorDisplay -Added functionality so that operations can be performed on the result of an other operation -Minor fixes

--- CalculatorDisplay.java
+++ CalculatorDisplay.java
@@ -12,20 +12,61 @@ class CalculatorDisplay extends HBox{
 
     private Text display = new Text("");
 
+    private Text displaySecondary = new Text("");
+
     CalculatorDisplay() {
 
         display.setFont(Font.font("sans serif", FontWeight.BOLD, 50));
 
+        displaySecondary.setFont(Font.font("sans serif", FontWeight.BOLD, 50));
+
         this.setAlignment(Pos.BOTTOM_RIGHT);
 
-        this.getChildren().add(display);
+        this.getChildren().addAll(display, displaySecondary);
     }
 
+    //Manually set display
     void setDisplay(String s) {
+        display.setFont(Font.font("sans serif", FontWeight.BOLD, 50));
         display.setText(s);
+        displaySecondary.setText("");
+
+    }
+
+    //Set display according to 'operand operator operand' notation
+    void setDisplay(double opOne, String operator, double opTwo) {
+        display.setFont(Font.font("sans serif", FontWeight.BOLD, 30));
+        display.setText(Double.toString(opOne) + " " + operator + "  ");
+
+        if (opTwo == 0) {
+            displaySecondary.setText("");
+        }
+        else {
+            displaySecondary.setText(Double.toString(opTwo));
+        }
+
+    }
+
+    //Set display according to 'Answer operator operand' notation
+    void setDisplay(String operator, double opTwo) {
+        display.setFont(Font.font("sans serif", FontWeight.BOLD, 30));
+        display.setText("ANS" + " " + operator + "  ");
+
+        if (opTwo == 0) {
+            displaySecondary.setText("");
+        }
+        else {
+            displaySecondary.setText(Double.toString(opTwo));
+        }
     }
 
     String getDisplay() {
         return display.getText();
+
+    }
+
+    void setDisplayFontSize(int n) {
+        display.setFont(Font.font(n));
+
     }
 }
