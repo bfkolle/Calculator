fc6842fda38f8d8fce359e4ff606d08005349bba
Brandon <bfkolle@live.com>
2018-08-02 19:27:19 -0400

    Completed implementation of the computation engine for addition, subtraction, division, and multiplication

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,16 +13,30 @@ import java.util.Collections;
 
 public class Calculator extends BorderPane
 {
+	private String infixExpression;
     private CalculatorDisplay display;
+    private ComputationEngine compEngine;
     private GridPane buttons;
-    private Button btCE, btC, btDel, btDiv, bt7, bt8, bt9, btMult,
+    private Button btCE, btC, btExp, btDiv, bt7, bt8, bt9, btMult,
 			bt4, bt5, bt6, btSub, bt1, bt2, bt3, btAdd, btNeg, bt0, btDec, btSolve;
+
 
     public Calculator()
 	{
 		ArrayList<Button> buttonHolder = new ArrayList<>();
+		infixExpression = new String();
 		display = new CalculatorDisplay();
 		buttons = new GridPane();
+		compEngine = new ComputationEngine();
+
+		//Declare each button of calculator
+        btCE = new Button("CE"); btC = new Button("C"); btExp = new Button("^");
+        btDiv = new Button("/"); bt7 = new Button("7"); bt8 = new Button("8");
+        bt9 = new Button("9"); btMult = new Button("x"); bt4 = new Button("4");
+        bt5 = new Button("5"); bt6 = new Button("6"); btSub = new Button("-");
+        bt1 = new Button("1"); bt2 = new Button("2"); bt3 = new Button("3");
+        btAdd = new Button("+"); btNeg = new Button("Neg"); bt0 = new Button("0");
+        btDec = new Button("."); btSolve = new Button("=");
 
 		//Add display to top
 		this.setTop(display);
@@ -35,16 +49,7 @@ public class Calculator extends BorderPane
 		this.setStyle("-fx-background-color: #353535");
 		this.setPadding(new Insets(0, 2.5, 0, 2.5));
 
-        //Declare each button of calculator
-        btCE = new Button("CE"); btC = new Button("C"); btDel = new Button("<-");
-        btDiv = new Button("/"); bt7 = new Button("7"); bt8 = new Button("8");
-        bt9 = new Button("9"); btMult = new Button("x"); bt4 = new Button("4");
-        bt5 = new Button("5"); bt6 = new Button("6"); btSub = new Button("-");
-        bt1 = new Button("1"); bt2 = new Button("2"); bt3 = new Button("3");
-        btAdd = new Button("+"); btNeg = new Button("Neg"); bt0 = new Button("0");
-        btDec = new Button("."); btSolve = new Button("=");
-
-        Collections.addAll(buttonHolder, btCE, btC, btDel, btDiv, bt7, bt8, bt9, btMult, bt4, bt5, bt6, btSub, bt1,
+        Collections.addAll(buttonHolder, btCE, btC, btExp, btDiv, bt7, bt8, bt9, btMult, bt4, bt5, bt6, btSub, bt1,
                 bt2, bt3, btAdd, btNeg, bt0, btDec, btSolve);
 
         //Set each button so it will grow to fit space
@@ -70,7 +75,7 @@ public class Calculator extends BorderPane
 		//Add buttons by column
 		buttons.addColumn(0, btCE, bt7, bt4, bt1, btNeg);
 		buttons.addColumn(1, btC, bt8, bt5, bt2, bt0);
-		buttons.addColumn(2, btDel, bt9, bt6, bt3, btDec);
+		buttons.addColumn(2, btExp, bt9, bt6, bt3, btDec);
 		buttons.addColumn(3, btDiv, btMult, btSub, btAdd, btSolve);
 	}
 }
--- src/ComputationEngine.java
+++ src/ComputationEngine.java
@@ -7,5 +7,124 @@ import java.util.ArrayDeque;
 
 public class ComputationEngine
 {
-	
+	public ComputationEngine()
+	{
+
+	}
+
+	public String computeExpression(String infixExpression)
+	{
+		ArrayDeque<String> stack = new ArrayDeque<>();
+		String postfixExpression = convertToPostfix(infixExpression).trim();
+
+		for(int i = 0; i < postfixExpression.length(); i++)
+		{
+			char temp = postfixExpression.charAt(i);
+
+			if(Character.isDigit(temp))
+			{
+				stack.push(Character.toString(temp));
+			}
+			else
+			{
+				String result;
+				double operandTwo = Double.parseDouble(stack.pop());
+				double operandOne = Double.parseDouble(stack.pop());
+
+				switch(temp)
+				{
+					case '+':
+						result = Double.toString(operandOne + operandTwo);
+						stack.push(result);
+						break;
+					case '-':
+						result = Double.toString(operandOne - operandTwo);
+						stack.push(result);
+						break;
+					case '*':
+						result = Double.toString(operandOne * operandTwo);
+						stack.push(result);
+						break;
+					case '/':
+						result = Double.toString(operandOne / operandTwo);
+						stack.push(result);
+						break;	
+					default:
+						break;
+				}
+			}
+		}
+		return stack.pop();
+	}
+
+	//Converts and infix expression to postfix
+	private String convertToPostfix(String infixExpression)
+	{
+		ArrayDeque<Character> stack = new ArrayDeque<>();
+		String postfixString = "";
+
+		//Convert the expression character by character
+		for(int i = 0; i < infixExpression.length(); i++)
+		{
+			char temp = infixExpression.charAt(i);
+
+			//Append to postfix string if the character is an operand
+			if(Character.isDigit(temp))
+			{
+				postfixString += temp;
+			}
+
+			//Push onto stack if the operator is a '('
+			else if(temp == '(')
+			{
+				stack.push(temp);
+			}
+
+			//If the operator is a ')' pop from stack until a '('
+			else if(temp == ')')
+			{
+				while(!stack.isEmpty() && stack.peek() != '(')
+				{
+					postfixString += stack.pop();
+				}
+
+				//Get rid of '('
+				stack.pop();
+			}
+
+			//The character is an operator
+			else
+			{
+				while(!stack.isEmpty() && getPrecedence(temp) <= getPrecedence(stack.peek()))
+				{
+					postfixString += stack.pop();
+				}
+				stack.push(temp);
+			}
+		}
+
+		//Pop all remaining operators from the stack
+		while(!stack.isEmpty())
+		{
+			postfixString += stack.pop();
+		}
+		return postfixString;
+	}
+
+	//Gets the precedence of an infix operator for postfix conversion
+	private int getPrecedence(char operator)
+	{
+			switch(operator)
+			{
+				case '+':
+				case '-':
+					return 1;
+				case '*':
+				case '/':
+					return 2;
+				case '^':
+					return 3;
+			}
+			return -1;
+	}
 }
